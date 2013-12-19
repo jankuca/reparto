7152e4809b53098bad03b2a285337bfa3a3c6de9
jankuca <jan@jankuca.com>
2013-12-19 20:03:55 +0100

    server retries TCP connections to machines on disconnect

--- app/cluster.js
+++ app/cluster.js
@@ -5,6 +5,7 @@ var Cluster = function (datagram_server, tcp) {
   this.tcp_ = tcp;
 
   this.machine_connections_ = {};
+  this.machine_statuses_ = {};
 };
 
 
@@ -25,18 +26,39 @@ Cluster.prototype.handleMessage_ = function (message, info) {
 
 
 Cluster.prototype.addMachine_ = function (address) {
-  var socket = this.machine_connections_[address];
-  if (!socket) {
+  var status = this.machine_statuses_[address];
+  if (!status) {
+    this.machine_statuses_[address] = 'new';
     this.connectToMachine_(address);
   }
 };
 
 
 Cluster.prototype.connectToMachine_ = function (address) {
+  var self = this;
   console.log('connect to ' + address);
 
   var socket = this.tcp_.connect(address);
   this.machine_connections_[address] = socket;
+  this.machine_statuses_[address] = 'connecting';
+
+  socket.on('connect', function () {
+    self.machine_statuses_[address] = 'up';
+    console.log('machines:', self.machine_statuses_);
+  });
+
+  socket.on('close', function (had_err) {
+    self.machine_statuses_[address] = 'down';
+    console.log('machines:', self.machine_statuses_);
+
+    if (!had_err) {
+      self.connectToMachine_(address);
+    }
+  });
+
+  socket.on('error', function (err) {
+    setTimeout(self.connectToMachine_.bind(self, address), 1000);
+  });
 };
 
 
