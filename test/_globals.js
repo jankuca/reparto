76dd6996dfe9e08fb874fcc78929912e542d0f7e
jankuca <jan@jankuca.com>
2013-12-24 11:14:34 +0100

    test async behavior of Cluster (monkey-patch setTimeout)

--- test/_globals.js
+++ test/_globals.js
@@ -1,4 +1,33 @@
+
 var log = require('../lib/log');
 log.silent = true;
 
+
 global.expect = require('expect.js');
+
+
+var timeout_queue = [];
+
+global.setTimeout = function (callback, timeout) {
+  return timeout_queue.push({
+    timeout: timeout,
+    callback: callback
+  });
+};
+
+global.clearTimeout = function (id) {
+  if (!id) return;
+  timeout_queue[id - 1] = null;
+};
+
+global.setTimeout.flush = function (ms) {
+  timeout_queue.forEach(function (item, index) {
+    if (!item) return;
+    if (typeof ms !== 'number' || item.timeout <= ms) {
+      timeout_queue[index] = null;
+      item.callback.call(null);
+    } else if (typeof ms === 'number') {
+      item.timeout -= ms;
+    }
+  });
+};
--- test/cluster.spec.js
+++ test/cluster.spec.js
@@ -135,8 +135,100 @@ describe('Cluster', function () {
     var datagram = new Datagram({ 'type': 'new-machine', 'tcp': 1234 });
     var info = { address: '123.1.1.1' };
     _message_listener(datagram, info);
-    _close_listener(false);
+    count = 0;
 
-    expect(count).to.be(2);
+    _close_listener(false);
+    setTimeout.flush();
+
+    expect(count).to.be(1);
+  });
+
+
+  it('should try to reconnect on connection error', function () {
+    var count = 0;
+    var _message_listener;
+    var _close_listener;
+    var _error_listener;
+
+    var socket = {
+      on: function (type, listener) {
+        if (type === 'close') {
+          _close_listener = listener;
+        }
+        if (type === 'error') {
+          _error_listener = listener;
+        }
+      }
+    };
+
+    var datagram_server = {
+      on: function (type, listener) { _message_listener = listener; }
+    };
+    var tcp = {
+      connect: function (address, port) {
+        count += 1;
+        return socket;
+      }
+    };
+    var cluster = new Cluster(datagram_server, tcp);
+    cluster.init();
+
+    var datagram = new Datagram({ 'type': 'new-machine', 'tcp': 1234 });
+    var info = { address: '123.1.1.1' };
+    _message_listener(datagram, info);
+    count = 0;
+
+    _error_listener();
+    _close_listener(true);
+    setTimeout.flush();
+
+    expect(count).to.be(1);
+  });
+
+
+  it('should try to reconnect after one second after connection failure',
+      function () {
+    var count = 0;
+    var _message_listener;
+    var _close_listener;
+    var _error_listener;
+
+    var socket = {
+      on: function (type, listener) {
+        if (type === 'close') {
+          _close_listener = listener;
+        }
+        if (type === 'error') {
+          _error_listener = listener;
+        }
+      }
+    };
+
+    var datagram_server = {
+      on: function (type, listener) { _message_listener = listener; }
+    };
+    var tcp = {
+      connect: function (address, port) {
+        count += 1;
+        return socket;
+      }
+    };
+    var cluster = new Cluster(datagram_server, tcp);
+    cluster.init();
+
+    var datagram = new Datagram({ 'type': 'new-machine', 'tcp': 1234 });
+    var info = { address: '123.1.1.1' };
+    _message_listener(datagram, info);
+    count = 0;
+
+    _error_listener();
+    _close_listener(true);
+    expect(count).to.be(0);
+
+    setTimeout.flush(900);
+    expect(count).to.be(0);
+
+    setTimeout.flush(100);
+    expect(count).to.be(1);
   });
 });
